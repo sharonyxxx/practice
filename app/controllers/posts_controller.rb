3e36a91a0b84398ad1e300e328a99f967578bcec
sharon <sharon.n13@gmail.com>
2016-02-29 16:47:58 +0800

    post before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,21 @@
 class PostsController < ApplicationController
+  before_action :find_group
+
+
   def new
-    @group = Group.find(params[:group_id])
+  #  @group = Group.find(params[:group_id])
     @post = @group.posts.new
   end
 
   def edit
-    @group = Group.find(params[:group_id])
+  #  @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
   end
 
 
 
   def create
-    @group = Group.find(params[:group_id])
+  #  @group = Group.find(params[:group_id])
     @post = @group.posts.build(post_params)
 
     if @post.save
@@ -23,7 +26,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:group_id])
+  #  @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
 
     if @post.update(post_params)
@@ -34,7 +37,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:group_id])
+
     @post = @group.posts.find(params[:id])
 
     @post.destroy
@@ -42,6 +45,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
