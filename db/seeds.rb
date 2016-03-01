8be6d095e246c226315defb699735174546e6307
sharon <sharon.n13@gmail.com>
2016-03-01 21:23:46 +0800

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+puts "Hello"
+
+create_account = User.create([email: 'example1@gmail.com', password: '12345678', password_confirmation: '12345678', name: '測試用帳號'])
+
+create_groups = for i in 1..20 do
+  Group.create!([title: "Group no.#{i}", description: "這是用種子建立的第 #{i} 個討論板", user_id: "1"])
+  GroupUser.create(group_id: i, user_id: 1)
+  for k in 1..30 do
+    Post.create!([group_id: "#{i}",content: "這是用種子建立的第#{k}個留言", user_id:"1"])
+  end
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
